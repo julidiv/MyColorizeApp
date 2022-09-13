fa5e8b162ad79c5f4205d01e0d2f9f3fd6f3ee30
julidiv <julia.dzivina@gmail.com>
2022-09-13 14:32:15 +0200

    Initial Commit

---
+++ MyColorizeApp/MyColorizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyColorizeAppApp.swift
+//  MyColorizeApp
+//
+//  Created by Alexander Golub on 13.09.22.
+//
+
+import SwiftUI
+
+@main
+struct MyColorizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
